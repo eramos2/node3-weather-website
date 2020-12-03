0da2573e9f579e2565debcdd0fc5c66b77fbfca4
Emmanuel Ramos <emmanuel.ramos2@ibm.com>
2020-12-03 00:57:39 -0400

    Added humidity data to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,8 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location, please add different coordinates', undefined)
         } else {
-            const { weather_descriptions, temperature, feelslike } = body.current
-            callback(undefined, weather_descriptions[0] + '. It is currently ' +  temperature + ' degrees out. It feels like ' + feelslike + '.')
+            const { weather_descriptions, temperature, feelslike, humidity } = body.current
+            callback(undefined, weather_descriptions[0] + '. It is currently ' +  temperature + ' degrees out. It feels like ' + feelslike + ' degrees out. The humidity is ' + humidity + '.'  )
         }
     })
 
